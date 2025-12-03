eddccabaa0c58b756026dac13b83154d14dabbb8
ThirdTempest <johnroben.manayon21@gmail.com>
2025-12-04 01:47:44 +0800

    typo

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->statefulApi();
-        $middleware->validateCsrfToken(except: [
+        $middleware->validateCsrfTokens(except: [
             'login',
             'register',
             'logout',
