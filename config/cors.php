d0d08ae9fb739795a087c94f22a391dc8a4e9c34
ThirdTempest <johnroben.manayon21@gmail.com>
2025-12-04 01:00:33 +0800

    config

--- config/cors.php
+++ config/cors.php
@@ -20,7 +20,7 @@ return [
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
-        env('FRONTEND_URL', 'http://localhost:9000'),
+        rtrim(env('FRONTEND_URL', 'http://localhost:9000'), '/'),
         'http://localhost:9000',
     ],
 
